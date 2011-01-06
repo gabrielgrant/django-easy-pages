b3df830ec2dbbf2e050b68eca1636830cae683bf
Gabriel Grant <g@briel.ca>
2011-01-06 05:21:46 -0500

    initial import of setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='django-easy-pages',
+    version='0.1.0dev',
+    author='Gabriel Grant',
+    packages=['easy_pages',],
+    license='LGPL',
+    long_description=open('README').read(),
+    install_requires=[
+        'django-inline-edit',
+        'django-ckeditor',
+        'django-ckeditor-filemodel-manager',
+        'django-html-field',
+        'django-static-filtered-images',
+    ],
+    dependency_links = [
+    	'http://github.com/gabrielgrant/django-inline-edit/tarball/master#egg=django-inline-edit',
+    	'http://github.com/gabrielgrant/django-ckeditor/tarball/master#egg=django-ckeditor',
+    	'http://github.com/gabrielgrant/django-ckeditor-filemodel-manager/tarball/master#egg=django-ckeditor-filemodel-manager',
+        'http://github.com/gabrielgrant/django-html-field/tarball/master#egg=django-html-field',
+        'http://github.com/gabrielgrant/django-static-filtered-images/tarball/master#egg=django-static-filtered-images',
+    ]
+)
+
