205f1334ce2cd69e363006850f91e5297d5b2fea
Gabriel Grant <g@briel.ca>
2011-01-06 09:09:46 -0500

    fixed bug with main_menu template tag

--- easy_pages/templatetags/easy_menus.py
+++ easy_pages/templatetags/easy_menus.py
@@ -11,7 +11,7 @@ def page_menu(page):
 
 @register.inclusion_tag('easy_pages/main_menu.html')
 def main_menu(depth, page=None):
-	if type(page) is not Page:
+	if type(page) is not Page and page is not None:
 		page = Page.objects.from_path(page)
 	return {'page': page, 'roots': Page.tree.root_nodes(), 'depth':int(depth)}
 
