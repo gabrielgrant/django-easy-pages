15253c33bcca21f177a52d9b6c4c48ed9dfe5496
Gabriel Grant <g@briel.ca>
2011-02-16 16:13:26 -0500

    adds a context processor to inject Pages; update README

---
+++ easy_pages/context_processors.py
@@ -0,0 +1,8 @@
+from easy_pages.models import Page
+
+def page(request):
+	""" returns the Page for the current request (if it exists)"""
+	try:
+		return {'page': Page.objects.from_path(request.path_info)}
+	except Page.DoesNotExist:
+		return {}
