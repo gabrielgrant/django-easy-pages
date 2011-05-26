e5d98a6fe52468d69899fe061079f49307961a08
Gabriel Grant <g@briel.ca>
2011-05-26 09:46:52 -0700

    fall back to top level (home: '/') page for main_menu lookup when no other ancestors are found

--- easy_pages/templatetags/easy_menus.py
+++ easy_pages/templatetags/easy_menus.py
@@ -15,7 +15,7 @@ def page_menu(page):
 @register.inclusion_tag('easy_pages/main_menu.html')
 def main_menu(depth, page=None):
 	if not isinstance(page, Page) and page is not None:
-		page = '/'.join(page.strip('/').split('/')[:depth+1])
+		page = '/'.join(page.rstrip('/').split('/')[:depth+1])
 		try:
 			page = Page.objects.from_path(page)
 		except Page.DoesNotExist:
