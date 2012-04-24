d35203d619875a2a5f4ed1fe0d771c09ff5b7e77
Gabriel Grant <g@briel.ca>
2012-04-23 19:57:41 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-easy-pages',
-    version='0.1.1',
+    version='0.1.2',
     author='Gabriel Grant',
     packages=['easy_pages',],
     license='LGPL',
