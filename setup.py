aa4cf26f8aecd7f8bd52a430968c3ed74df791b0
Gabriel Grant <g@briel.ca>
2021-04-15 03:37:53 -0400

    update setup.py to use the PEP 508 syntax to specify package URLs instead of dependency_links
    details: https://stackoverflow.com/questions/50498672/setup-py-dependency-links-not-searched-for-install-requires

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     long_description=open('README').read(),
     install_requires=[
         'django-mptt',
-        'django-inline-edit',
-        'django-ckeditor',
-        'django-ckeditor-filemodel-manager',
-        'django-html-field',
-        'django-static-filtered-images',
-        'django-python-identifier-field',
+        'django-inline-edit @ http://github.com/gabrielgrant/django-inline-edit/tarball/master#egg=django-inline-edit',
+        'django-ckeditor @ http://github.com/gabrielgrant/django-ckeditor/tarball/master#egg=django-ckeditor',
+        'django-ckeditor-filemodel-manager @ http://github.com/gabrielgrant/django-ckeditor-filemodel-manager/tarball/master#egg=django-ckeditor-filemodel-manager',
+        'django-html-field @ http://github.com/gabrielgrant/django-html-field/tarball/master#egg=django-html-field',
+        'django-static-filtered-images @ http://github.com/gabrielgrant/django-static-filtered-images/tarball/master#egg=django-static-filtered-images',
+        'django-python-identifier-field @ http://github.com/gabrielgrant/django-python-identifier-field/tarball/master#egg=django-python-identifier-field',
     ],
     dependency_links = [
     	'http://github.com/gabrielgrant/django-inline-edit/tarball/master#egg=django-inline-edit',
